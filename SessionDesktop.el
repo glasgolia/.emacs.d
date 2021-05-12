fa62efcd305828c6f6fdc9eea1927145f670cb0c
Peter Muys <peter@petermuys.com>
2021-05-12 16:37:54 +0200

    Initial commit

---
+++ SessionDesktop.el
@@ -0,0 +1,33 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Tue Mar 30 15:03:02 2021
+;; Desktop file format version 208
+;; Emacs version 25.3.50.1
+(setq revive:frame-configuration-to-restore
+  '(progn ))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (24675 8582 720330 0) (desktop . "208") "petermuys@ernie.local" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Menlo-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "#e1e1e0") (background-color . "#2d3743") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . dark) (cursor-color . "Red") (visibility) (frameset--id . "86EE-C164-0974-D693") (frameset--mini t . t) (height . 83) (width . 179) (modeline . t) (minibuffer . t) (unsplittable) (left . 2963) (top + -365) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "ernie.local") (explicit-name . t) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 6) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 56) (min-pixel-width . 70) (min-pixel-height-ignore . 28) (min-pixel-width-ignore . 38) (min-pixel-height-safe . 14) (min-pixel-width-safe . 14)) leaf (pixel-width . 1277) (pixel-height . 1148) (total-width . 182) (total-height . 82) (normal-height . 1.0) (normal-width . 1.0) (buffer " *empty*" (selected . t) (hscroll . 0) (fringes 1 8 nil) (margins nil) (scroll-bars nil 3 t nil 0 nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist '((48 . "https://github.com/flyingmachine/emacs-for-clojure")))
+(setq file-name-history '("~/.emacs.d/README.md" "~/.emacs.d/init.el" "~/.emacs.d/" "~/Library/Preferences/Aquamacs Emacs/customizations.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/cider-0.24.0/cider-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/clojure-mode-20200321.1035/clojure-mode-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/parseedn-20191113.831/parseedn-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/parseclj-20190531.711/parseclj-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/a-20180907.953/a-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/pkg-info-20150517.1143/pkg-info-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/epl-20180205.2049/epl-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/queue-0.2/queue-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/spinner-1.7.3/spinner-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/seq-2.20/seq-autoloads.el" "~/Library/Preferences/Aquamacs Emacs/Packages/elpa/sesman-20190909.1754/sesman-autoloads.el" "~/aa/dev/goliac/project.clj"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 208
+  "/Users/petermuys/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode)
+  19
+  '(12 nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8-unix) (truncate-lines))
+  '((mark-ring nil)))
+
